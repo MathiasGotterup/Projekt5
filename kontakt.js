465b4c29801a63b0d243741fe7aa73af5c87e83b
MathiasGotterup <mat-g@live.dk>
2022-04-20 09:49:12 +0200

    d

---
+++ kontakt.js
@@ -0,0 +1,22 @@
+function _(id) {return document.getElementById(id);}
+function submitForm(){
+	_("mybtn").disabled = true;
+	_("status").innerHTML = 'please wait ...';
+	var formdata = new FormData();
+	formdata.append( "n", _("n").value );
+	formdata.append( "e", _("e").value );
+  formdata.append( "tf", _("tf").value );
+	formdata.append( "m", _("m").value );
+	var ajax = new XMLHttpRequest();
+	ajax.open( "POST", "example_parser.php" );
+	ajax.onreadystatechange = function() {
+		if(ajax.readyState == 4 && ajax.status == 200) {
+			if(ajax.responseText == "success"){
+				_("my_form").innerHTML = '<h2>Tak '+_("n").value+', din besked er blevet sendt.</h2>';
+			} else {
+				_("status").innerHTML = ajax.responseText;
+				_("mybtn").disabled = false;
+			}
+		}
+	}
+	ajax.send( formdata );}
