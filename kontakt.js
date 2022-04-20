3fb9984bc43c5fc5d84ab2ae3d1e8218a6f0c377
MathiasGotterup <mat-g@live.dk>
2022-04-20 12:35:01 +0200

    pislort

--- kontakt.js
+++ kontakt.js
@@ -1,22 +1,31 @@
-function _(id) {return document.getElementById(id);}
-function submitForm(){
-	_("mybtn").disabled = true;
-	_("status").innerHTML = 'please wait ...';
-	var formdata = new FormData();
-	formdata.append( "n", _("n").value );
-	formdata.append( "e", _("e").value );
-  formdata.append( "tf", _("tf").value );
-	formdata.append( "m", _("m").value );
-	var ajax = new XMLHttpRequest();
-	ajax.open( "POST", "example_parser.php" );
-	ajax.onreadystatechange = function() {
-		if(ajax.readyState == 4 && ajax.status == 200) {
-			if(ajax.responseText == "success"){
-				_("my_form").innerHTML = '<h2>Tak '+_("n").value+', din besked er blevet sendt.</h2>';
-			} else {
-				_("status").innerHTML = ajax.responseText;
-				_("mybtn").disabled = false;
-			}
-		}
-	}
-	ajax.send( formdata );}
+let navnInput = document.querySelector("#navn");
+let email = document.querySelector("#email");
+let telefon = document.querySelector("#telefon");
+let besked = document.querySelector("#besked");
+let sendt = document.querySelector("#sendt");
+let errorFejl = document.querySelector(".error");
+
+function validateForm() {
+
+  clearBesked();
+
+  if(navnInput.value.length < 1) {
+    errorFejl[0].innerText = "Name cannot be blank";
+    navnInput.classList.add("error-bordor")
+
+  }
+  if(!emailIsValid(email.valid)) {
+    errorFejl[1].innerText = "Invalid Email";
+    navnInput.classList.add("error-bordor")
+}
+function clearBesked() {
+  for (var i = 0; i < errorFejl.length; i++) {
+    errorFejl[i].innerText = "";
+  }
+  navnInput.classList.remove("error-bordor")
+}
+
+function emailIsValid(email) {
+  let
+
+}
