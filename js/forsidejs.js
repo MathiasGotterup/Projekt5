3e1c11bf8b53b0fcb2b7dbf3066ae5dc9eaad8d5
jacobjuhl2021 <jacobjuhlprivat@gmail.com>
2022-04-23 17:27:01 +0200

    notering til java

--- js/forsidejs.js
+++ js/forsidejs.js
@@ -1,23 +1,34 @@
 // Jacob
 
 let slideIndex = 1;
+
+//Viser billedet når siden startes
 showSlides(slideIndex);
 
-// Next/previous controls
+// Frem og tilbage knapperne i siden virker
 function plusSlides(n) {
+  //slideIndex += n giver følgende: slideIndex = slideIndex + n
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
+  //slideIndex = n giver følgende: slideIndex = n
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
+  // Tilkændegiver variablen "i" til senere ændring
   let i;
+
+  //Fortæller at variablen "slides" findes fra HTML classen "mySlides"
   let slides = document.getElementsByClassName("mySlides");
+  //Fortæller at variablen "dots" findes fra HTML classen "dot"
   let dots = document.getElementsByClassName("dot");
+
+  //slides.length = array
   if (n > slides.length) {slideIndex = 1;}
+  
   if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
